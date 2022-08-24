1a86fb4375812c51c6e97e9ed567fac2f284cba3
MK-Khan123 <mehnazkhan231@gmail.com>
2022-08-25 01:02:32 +0600

    13. Teleporting Elements

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -3,11 +3,14 @@
     <h2>Manage Goals</h2>
     <input type="text" ref="goal" />
     <button @click="setGoal">Set Goal</button>
-    <error-alert v-if="inputIsInvalid">
+
+    <teleport to="body">
+      <error-alert v-if="inputIsInvalid">
         <h2>Input is invalid!</h2>
         <p>Please enter at least a few characters ...</p>
         <button @click="confirmError">Okay</button>
-    </error-alert>
+      </error-alert>
+    </teleport>
   </div>
 </template>
 
@@ -31,8 +34,8 @@ export default {
       }
     },
     confirmError() {
-        this.inputIsInvalid = false;
-    }
+      this.inputIsInvalid = false;
+    },
   },
 };
 </script>
